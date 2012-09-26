eb38a9606fd9832aa07eb0953731eade725a3533
KirillBel <kirill_bel@mail.ru>
2012-09-27 00:20:15 +0400

    Добавлен пустой класс FieldSim.

---
+++ hdr/FieldSim.h
@@ -0,0 +1,16 @@
+/* 
+ * File:   FieldSim.h
+ * Author: kirill
+ *
+ * Created on September 26, 2012, 9:54 PM
+ */
+
+#ifndef _FIELDSIM_H
+#define	_FIELDSIM_H
+
+class FieldSim {
+
+};
+
+#endif	/* _FIELDSIM_H */
+
---
+++ src/FieldSim.cpp
@@ -0,0 +1,9 @@
+/* 
+ * File:   FieldSim.cpp
+ * Author: kirill
+ * 
+ * Created on September 26, 2012, 9:54 PM
+ */
+
+#include "FieldSim.h"
+
